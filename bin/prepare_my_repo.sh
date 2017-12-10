ee8cdd11abf3392e2b279a564f8cd2ca394030e4
Tamsi <Tamsi@users.noreply.github.com>
2017-12-10 02:19:24 +0100

    Update prepare_my_repo.sh

--- bin/prepare_my_repo.sh
+++ bin/prepare_my_repo.sh
@@ -2,3 +2,5 @@ blih -u tamsi.besson@epitech.eu repository create "$1"
 blih -u tamsi.besson@epitech.eu repository setacl "$1" ramassage-tek r
 blih -u tamsi.besson@epitech.eu repository getacl "$1"
 git clone git@git.epitech.eu:/tamsi.besson@epitech.eu/"$1"
+#ca sert a upload la sshkey publique
+#python blih.py -u tamsi.besson@epitech.eu sshkey upload ~/.ssh/id_rsa.pub
